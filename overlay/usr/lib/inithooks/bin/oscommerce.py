8c5ef0d0bd5edcba59ae293f6bbf39d92e28e4bd
Stefan Davis <stefan@turnkeylinux.org>
2018-06-27 11:06:00 +1000

    Update osCommerce to 2.3.4.1
    - Enabled SSL by default
    - Now requires domain to be set

--- overlay/usr/lib/inithooks/bin/oscommerce.py
+++ overlay/usr/lib/inithooks/bin/oscommerce.py
@@ -4,11 +4,14 @@
 Option:
     --pass=     unless provided, will ask interactively
     --email=    unless provided, will ask interactively
+    --domain=   unless provided, will ask interactively
 
 """
 
+import re
 import sys
 import getopt
+import urlparse
 import inithooks_cache
 import subprocess
 from subprocess import PIPE
@@ -17,6 +20,10 @@ from os.path import *
 from dialog_wrapper import Dialog
 from mysqlconf import MySQL
 
+def fatal(s):
+    print >> sys.stderr, "Error:", s
+    sys.exit(1)
+
 def usage(s=None):
     if s:
         print >> sys.stderr, "Error:", s
@@ -24,15 +31,18 @@ def usage(s=None):
     print >> sys.stderr, __doc__
     sys.exit(1)
 
+DEFAULT_DOMAIN="www.example.com"
+
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
-                                       ['help', 'pass=', 'email='])
+                                       ['help', 'pass=', 'email=', 'domain='])
     except getopt.GetoptError, e:
         usage(e)
 
     password = ""
     email = ""
+    domain = ""
     for opt, val in opts:
         if opt in ('-h', '--help'):
             usage()
@@ -40,6 +50,8 @@ def main():
             password = val
         elif opt == '--email':
             email = val
+        elif opt == '--domain':
+            domain = val
 
     if not password:
         d = Dialog('TurnKey Linux - First boot configuration')
@@ -58,6 +70,30 @@ def main():
 
     inithooks_cache.write('APP_EMAIL', email)
 
+    if not domain:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        domain = d.get_input(
+            "osCommerce Domain",
+            "Enter the domain to serve osCommerce",
+            DEFAULT_DOMAIN)
+        
+    if domain == "DEFAULT":
+        domain = DEFAULT_DOMAIN
+
+    parsed = urlparse.urlparse(domain)
+    if not all([parsed.scheme, parsed.netloc, parsed.params, parsed.query, parsed.fragment]) and parsed.path:
+        # for a correctly formatted domain the prefixed `//` is not necessary making urlparse assume its a
+        # path rather than a netloc
+        domain = parsed.path
+    else:
+        # for a correctly formatted url (not actually what we want), we can safely extract the netloc
+        domain = parsed.netloc
+
+    inithooks_cache.write('APP_DOMAIN', domain)
+    
+
     command = ["php", join(dirname(__file__), 'oscommerce_pass.php'), password]
     p = subprocess.Popen(command, stdin=PIPE, stdout=PIPE, shell=False)
     stdout, stderr = p.communicate()
@@ -73,6 +109,31 @@ def main():
     m.execute('UPDATE oscommerce.configuration SET configuration_value=\"%s\" WHERE configuration_key=\"STORE_OWNER_EMAIL_ADDRESS\";' % email)
     m.execute('UPDATE oscommerce.configuration SET configuration_value=\"%s\" WHERE configuration_key=\"MODULE_PAYMENT_PAYPAL_EXPRESS_SELLER_ACCOUNT\";' % email)
 
+    conf_paths = (
+        '/var/www/oscommerce/admin/includes/configure.php',
+        '/var/www/oscommerce/includes/configure.php'
+    )
+
+    for conf_path in conf_paths:
+        with open(conf_path, 'r') as fob:
+            conf = fob.read().splitlines()
+
+
+        for i, line in enumerate(conf[:]):
+            if 'HTTPS_SERVER' in line:
+                conf[i] = re.sub(
+                    r"define\('HTTPS_SERVER', '.*'\);",
+                    "define('HTTPS_SERVER', 'https://%s');" % domain,
+                    line)
+            if 'HTTP_SERVER' in line:
+                conf[i] = re.sub(
+                    r"define\('HTTP_SERVER', '.*'\);",
+                    "define('HTTP_SERVER', 'http://%s');" % domain,
+                    line)
+            print(line, conf[i])
+
+        with open(conf_path, 'w') as fob:
+            fob.write('\n'.join(conf))
 
 if __name__ == "__main__":
     main()
