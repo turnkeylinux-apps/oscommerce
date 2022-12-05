207543ac15967ae034d100601408f5c9cc037409
Stefan Davis <stefan@turnkeylinux.org>
2022-12-05 01:28:30 +0000

    update for 17.0

--- overlay/usr/lib/inithooks/bin/oscommerce.py
+++ overlay/usr/lib/inithooks/bin/oscommerce.py
@@ -14,7 +14,6 @@ import getopt
 import urllib.parse
 from libinithooks import inithooks_cache
 import subprocess
-from subprocess import PIPE
 from os.path import *
 
 from libinithooks.dialog_wrapper import Dialog
@@ -84,46 +83,46 @@ def main():
 
     inithooks_cache.write('APP_DOMAIN', domain)
 
-    command = ["php", join(dirname(__file__), 'oscommerce_pass.php'), password]
-    p = subprocess.Popen(command, stdin=PIPE, stdout=PIPE, shell=False)
-    stdout, stderr = p.communicate()
-    if stderr:
-        fatal(stderr)
-
-    cryptpass = stdout.strip()
+    hashed = {}
+    for line in subprocess.run(
+	["php", join(dirname(__file__), 'oscommerce_pass.php'), password, email],
+	text=True,
+	capture_output=True
+    ).stdout.strip().splitlines():
+        key, value = line.split(' ', 1)
+        hashed[key] = value
 
     m = MySQL()
-    m.execute('UPDATE oscommerce.administrators SET user_password=%s WHERE user_name=\"admin\";', (cryptpass,))
+    m.execute('UPDATE oscommerce.admin SET admin_password=%s, admin_email_address=%s, admin_email_token=%s WHERE admin_username=\"admin\";', (
+        hashed['password'],
+        email,
+        hashed['email']
+    ))
 
     m.execute('UPDATE oscommerce.configuration SET configuration_value=%s WHERE configuration_key=\"EMAIL_FROM\";', (email,))
     m.execute('UPDATE oscommerce.configuration SET configuration_value=%s WHERE configuration_key=\"STORE_OWNER_EMAIL_ADDRESS\";', (email,))
     m.execute('UPDATE oscommerce.configuration SET configuration_value=%s WHERE configuration_key=\"MODULE_PAYMENT_PAYPAL_EXPRESS_SELLER_ACCOUNT\";', (email,))
 
-    conf_paths = (
-        '/var/www/oscommerce/admin/includes/configure.php',
-        '/var/www/oscommerce/includes/configure.php'
-    )
+    conf_path = '/var/www/oscommerce/admin/includes/local/configure.php'
 
-    for conf_path in conf_paths:
-        with open(conf_path, 'r') as fob:
-            conf = fob.read().splitlines()
+    with open(conf_path, 'r') as fob:
+        conf = fob.read().splitlines()
 
 
-        for i, line in enumerate(conf[:]):
-            if 'HTTPS_SERVER' in line:
-                conf[i] = re.sub(
-                    r"define\('HTTPS_SERVER', '.*'\);",
-                    "define('HTTPS_SERVER', 'https://%s');" % domain,
-                    line)
-            if 'HTTP_SERVER' in line:
-                conf[i] = re.sub(
-                    r"define\('HTTP_SERVER', '.*'\);",
-                    "define('HTTP_SERVER', 'http://%s');" % domain,
-                    line)
-            print((line, conf[i]))
+    for i, line in enumerate(conf[:]):
+        if 'HTTPS_SERVER' in line:
+            conf[i] = re.sub(
+                r"define\('HTTPS_SERVER', '.*'\);",
+                "define('HTTPS_SERVER', 'https://%s');" % domain,
+                line)
+        if 'HTTP_SERVER' in line:
+            conf[i] = re.sub(
+                r"define\('HTTP_SERVER', '.*'\);",
+                "define('HTTP_SERVER', '');",
+                line)
 
-        with open(conf_path, 'w') as fob:
-            fob.write('\n'.join(conf))
+    with open(conf_path, 'w') as fob:
+        fob.write('\n'.join(conf))
 
     apache_conf = "/etc/apache2/sites-available/oscommerce.conf"
     subprocess.run(["sed", "-i", "\|RewriteRule|s|https://.*|https://%s/\$1 [R,L]|" % domain, apache_conf])
